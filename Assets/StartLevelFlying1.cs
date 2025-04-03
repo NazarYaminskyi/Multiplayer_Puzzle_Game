e3b54746628257a98962bd552cd6441030164422
Lucky-L1zard <sansangol777@gmail.com>
2025-04-03 19:12:31 +0300

    1st Level Update
    Added some Environmant (decorations) , border(that respawn Player) , Flying Platform and Level`s Tilemap was some updated.

---
+++ Assets/StartLevelFlying1.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class StartLevelFlying1 : MonoBehaviour
+{
+    public float startpoint = -8.45f;
+    public float endpoint = -5.45f;
+    [SerializeField] private float speed = 5;
+    private Vector2 direction = Vector2.up;
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        if (transform.position.y <= endpoint)
+        {
+            direction = Vector2.up;
+        }
+        else if (transform.position.y >= startpoint)
+        {
+            direction = Vector2.down;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player2")
+        {
+            PlayerController2DD player = other.transform.GetComponent<PlayerController2DD>();
+            player.Death();
+            Debug.Log("Destroyed!");
+        }
+    }
+}
+
